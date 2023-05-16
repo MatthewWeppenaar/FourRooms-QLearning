3878d90ff9e11b87c90998704d972f6b6455017b
Matthew <wppmat001@myuct.ac.za>
2023-05-16 09:24:36 +0200

    added stocatic flag

--- Scenario1.py
+++ Scenario1.py
@@ -1,67 +1,87 @@
 from FourRooms import FourRooms
 import numpy as np
 import random
+import time
+import sys
+
 
 def convertTo1D(n):
-    #takes in a tuple(x,y) and it converts it into a 1d index for the Q-table
-    postion = n[0]*12+n[1]
-    return postion
+    # takes in a tuple (x, y) and converts it into a 1D index for the Q-table
+    position = n[0] * 12 + n[1]
+    return position
 
 def main():
-
     # Create FourRooms Object
-    fourRoomsObj = FourRooms('simple')
+    if len(sys.argv) == 2:
+        if sys.argv[1] == "-stochastic":
+            fourRoomsObj = FourRooms('simple',stochastic="True")
+    else:
+        fourRoomsObj = FourRooms('simple')
 
-    #hyper-parameters
-    learning_rate = 0.1
-    discount_factor = 0.6
-    exploration = 0.01# very low exploration rate
+    # Hyperparameters
+    learning_rate = 0.3
+    discount_factor = 0.9
+    exploration = 0.1  # very low exploration rate
     epochs = 10000
 
-    #creating a q-table, given set of states and set of actions
-    q_table = np.zeros([12*12, 4])
-
+    # Creating a Q-table, given the set of states and set of actions
+    q_table = np.zeros([12 * 12, 4])
 
     print('Agent starts at: {0}'.format(fourRoomsObj.getPosition()))
+    start_time = time.time()
 
     for trip in range(epochs):
-         #reset environemt
-         fourRoomsObj.newEpoch()
-         #set initial terminal state to false
-         isTerminal = False
-         
-         while not isTerminal:
-            #getting a random value to decide between exploration or exploitation
-            random_value = random.uniform(0,1)
-            #exploration
-            if (random_value < exploration):
-                action = random.randint(0,3)# Explore a random action
-            #exploitation
-            else:
-                #getting 'reward' from q-table
-                action = np.argmax(q_table[convertTo1D(fourRoomsObj.getPosition())])
+        fourRoomsObj.newEpoch()
+        state = fourRoomsObj.getPosition()
+        # Reset environment
+        
+        # Set initial terminal state to False
+        isTerminal = False
+        
 
-            #creating a tempory variable to old the current state
-            current_position = convertTo1D(fourRoomsObj.getPosition())
-            
-            #getting a new position
+        while not isTerminal:
+            # Getting a random value to decide between exploration or exploitation
+            random_value = random.uniform(0, 1)
+            # Exploration
+            if random_value < exploration:
+                action = random.randint(0, 3)  # Explore a random action
+            # Exploitation
+            else:
+                # Getting 'reward' from q-table
+                action = np.argmax(q_table[convertTo1D(state)])
+
+            # Creating a temporary variable to hold the current state
+            current_position = convertTo1D(state)
+
+            # Getting a new position
             gridType, newPos, packagesRemaining, isTerminal = fourRoomsObj.takeAction(action)
 
-            #added reward function
+            # Added reward function
             reward = 0
-            #if the package is collected we reward agent with 100
+            # If the package is collected, we reward the agent with 100
             if packagesRemaining == 0:
-                reward = 100
-            #else we penalize for each extra step
+                reward += 50
+            # Else we penalize for each extra step
             else:
-                reward -=10
-            #updating q-table
+                reward -= 1
+
+            
+            # Updating q-table
             prev_q = q_table[current_position, action]
             next_max_q = np.max(q_table[convertTo1D(newPos)])
             new_q = (1 - learning_rate) * prev_q + learning_rate * (reward + discount_factor * next_max_q)
             q_table[current_position, action] = new_q
-    #showing the final path
-    fourRoomsObj.showPath(-1)
+
+            state = newPos
+    # Showing the final path
+    
+    print("Time taken to run {0} Epochs".format(epochs))
+    print("--- %s seconds ---" % (time.time() - start_time))
+    if len(sys.argv) == 2:
+        if sys.argv[1] == "-stochastic":
+            fourRoomsObj.showPath(-1,"Scenario_1(stochastic).png")
+    else:
+        fourRoomsObj.showPath(-1,"Scenario_1.png")
 
 
 if __name__ == "__main__":
